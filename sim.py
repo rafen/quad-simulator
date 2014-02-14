655169059b3b3f3a9eccc21bb85ad661c3b52f04
Rafael Capdevielle <capdevielle@gmail.com>
2014-02-14 19:03:36 -0300

    Added control by analog command, using accelerators from wii mote

--- sim.py
+++ sim.py
@@ -11,10 +11,16 @@ class Stable(object):
         # set max and min forces
         self.max_force = max_force
         self.min_force = min_force
-        # Aceleration set points
-        self.set_point_aceleration = 0
-        self.aceleration = PID(4, 0.8, 2)
-        self.aceleration.setPoint(self.set_point_aceleration)
+        # commands
+        self.command_pitch = 0
+        self.command_roll = 0
+        self.command_yaw = 0
+        self.command_acceleration = 0
+
+        # acceleration set points
+        self.set_point_acceleration = 0
+        self.acceleration = PID(4, 0.8, 2)
+        self.acceleration.setPoint(self.set_point_acceleration)
         # Pitch set point
         self.set_point_pitch = 0
         self.pitch = PID(0.02, 0.001, 0.10)
@@ -28,31 +34,19 @@ class Stable(object):
         self.yaw = PID(0.2, 0.001, 1)
         self.yaw.setPoint(self.set_point_yaw)
 
-    def set_aceleration(self, aceleration):
-        if self.set_point_aceleration != aceleration:
-            self.set_point_aceleration = aceleration
-            self.aceleration(aceleration)
+    def update_forces(self, forces):
+        # Update forces in min and max values
+        for i, force in enumerate(forces):
+            force = max(self.min_force, force)
+            force = min(self.max_force, force)
+            forces[i] = force
+        # set values to quad
+        self.quad.motors_force = forces
 
-    def set_pitch(self, pitch):
-        if self.set_point_pitch != pitch:
-            self.set_point_pitch = pitch
-            self.pitch.setPoint(self.set_point_pitch)
-
-    def set_roll(self, roll):
-        if self.set_point_roll != roll:
-            self.set_point_roll = roll
-            self.roll.setPoint(self.set_point_roll)
-
-    def set_yaw(self, yaw):
-        if self.set_point_yaw != yaw:
-            self.set_point_yaw = yaw
-            self.yaw.setPoint(self.set_point_yaw)
-
-    def update(self):
-        self.quad.pos
-        forces = []
-        # aceleration
-        aforce = self.aceleration.update(self.quad.vel[1])
+    def stabilize(self):
+        forces = [0, 0, 0, 0]
+        # acceleration
+        aforce = self.acceleration.update(self.quad.vel[1])
         forces = [aforce, aforce, aforce, aforce]
         # pitch
         pforce = self.pitch.update(self.quad.pitch)
@@ -69,16 +63,24 @@ class Stable(object):
         forces[2] -= yforce
         forces[3] -= yforce
 
-        # Update forces in min and max values
-        for i, force in enumerate(forces):
-            force = max(self.min_force, force)
-            force = min(self.max_force, force)
-            forces[i] = force
-        # set values to quad
-        self.quad.motors_force = forces
+        self.update_forces(forces)
+
+    def apply_commands(self):
+        if self.set_point_acceleration != self.command_acceleration:
+            self.set_point_acceleration = -self.command_acceleration
+            self.acceleration.setPoint(self.set_point_acceleration)
+
+        forces = [
+            self.quad.motors_force[0] + self.command_pitch + self.command_yaw,
+            self.quad.motors_force[1] + -self.command_pitch + self.command_yaw,
+            self.quad.motors_force[2] + self.command_roll - self.command_yaw,
+            self.quad.motors_force[3] + -self.command_roll - self.command_yaw,
+        ]
+        self.update_forces(forces)
 
     def step(self):
-        self.update()
+        self.stabilize()
+        self.apply_commands()
         self.quad.step()
 
 
@@ -98,7 +100,7 @@ if __name__ == '__main__':
     stable = Stable(quad)
 
     # Initial noise
-    quad.motors[2].addForce((0.5, 1, 0))
+    #quad.motors[2].addForce((0.5, 1, 0))
 
     # Do the simulation...
     total_time = 0.0
@@ -109,34 +111,29 @@ if __name__ == '__main__':
         # Control Quad if controls are enabled
         if wm:
             if wm.state['buttons'] & cwiid.BTN_UP:
-                stable.set_roll(1)
+                stable.command_acceleration = 5
             if wm.state['buttons'] & cwiid.BTN_DOWN:
-                stable.set_roll(-1)
-            if wm.state['buttons'] & cwiid.BTN_LEFT:
-                stable.set_pitch(1)
-            if wm.state['buttons'] & cwiid.BTN_RIGHT:
-                stable.set_pitch(-1)
-            if wm.state['buttons'] & cwiid.BTN_RIGHT:
-                stable.set_pitch(-1)
+                stable.command_acceleration = -5
             if wm.state['buttons'] & cwiid.BTN_1:
-                stable.set_altitude(-0.5)
+                stable.command_acceleration = 1
             if wm.state['buttons'] & cwiid.BTN_2:
-                stable.set_altitude(1)
+                stable.command_acceleration = -1
             if wm.state['buttons'] & cwiid.BTN_A and wm.state['acc']:
-                # apply noise to motors directly
-                n1 = (wm.state['acc'][0] - 125.) / 125.
-                n2 = (wm.state['acc'][1] - 125.) / 125.
-                quad.motors[0].addForce((0, n1, 0))
-                quad.motors[2].addForce((0, n2, 0))
-                print 'noise', n1, n2
+                p = (wm.state['acc'][1] - 125.) / 125.
+                r = (wm.state['acc'][0] - 125.) / 125.
+                stable.command_pitch = -p
+                stable.command_roll = r
+            if wm.state['buttons'] & cwiid.BTN_B and wm.state['acc']:
+                a = (wm.state['acc'][1] - 125.) / 125.
+                stable.command_acceleration = a * 10
             if wm.state['buttons'] & cwiid.BTN_HOME:
                 wm.close()
                 break
             if not wm.state['buttons']:
-                stable.set_aceleration(0)
-                stable.set_pitch(0)
-                stable.set_roll(0)
-                stable.set_way(0)
+                stable.command_pitch = 0
+                stable.command_roll = 0
+                stable.command_yaw = 0
+                stable.command_acceleration = 0
 
         print "%1.2fsec: pos=(%6.3f, %6.3f, %6.3f) vel=(%6.3f, %6.3f, %6.3f) pry=(%6.3f, %6.3f, %6.3f)" % \
               (total_time, quad.pos[0], quad.pos[1], quad.pos[2],
